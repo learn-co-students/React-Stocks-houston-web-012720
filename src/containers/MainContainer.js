1a2eb3656d32095434747262e1a000691111ae1a
somaia-khalil <sumaya_khair@live.com>
2020-04-04 22:12:15 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,21 +4,84 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  constructor(){
+    super()
+    this.state = {
+      stocks: [],
+      a: [],
+      displayArray: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(stocks => this.setState({
+      stocks:stocks,
+      displayArray: stocks
+
+    }))
+  }
+
+  addToport = (stock) => {
+    // debugger
+    this.setState({a: [...this.state.a, stock]})
+  }
+
+  deletefromport = (stocka) => {
+    let notdeleteditem = this.state.a.filter(stock => stock !==stocka)
+    this.setState({
+      a: notdeleteditem
+    })
+  }
+
+  sortA=()=>{
+    let sortme = [...this.state.stocks]
+    // debugger
+    let sortedarray = sortme.sort((a,b)=>a.name > b.name ? 1:-1)
+    //  console.log(sortedarray)
+    this.setState({
+      displayArray: sortedarray
+      
+    }, ()=>console.log(this.state.stocks))
+  }
+  sortP=()=>{
+    let sortme = [...this.state.stocks]
+    // debugger
+    let sortedarray = sortme.sort((a,b)=>a.price > b.price ? 1:-1)
+  
+    this.setState({
+      displayArray: sortedarray
+      
+    }, ()=>console.log(this.state.stocks))
+  }
+
+  filter = (event) => {
+    console.log(event)
+    let filterme = [...this.state.stocks]
+    let filterarray = filterme.filter(stock => stock.type == event)
+    this.setState({
+      displayArray: filterarray
+    })
+  }
+
+
+ 
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortA={this.sortA} sortP={this.sortP} filter={this.filter}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.displayArray} addToport={this.addToport}  a={this.state.a} deletefromport={this.deletefromport} />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer a={this.state.a} deletefromport={this.deletefromport} />
 
             </div>
           </div>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -4,11 +4,12 @@ import Stock from '../components/Stock'
 class PortfolioContainer extends Component {
 
   render() {
+     console.log(this.props.a)
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.props.a.map(stock => <Stock stock={stock} clicked={this.props.deletefromport} />)
           }
       </div>
     );

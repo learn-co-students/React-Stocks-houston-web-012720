c2c59432599caa6ef3dd93c50603eecdfe0ad34a
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-04-06 17:16:45 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,56 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  constructor(){
+    super()
+    this.state = {
+      stocks: [],
+      portfolio: [],
+      filter: "",
+      sortAlphabetically: true
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/stocks")
+      .then(resp => resp.json())
+      .then(json => this.setState({stocks: json}))
+  }
+
+  buyStock = (stock) => this.setState({portfolio: [...this.state.portfolio.filter(s => s.id !== stock.id), stock]})
+  sellStock = (stock) => this.setState({portfolio: this.state.portfolio.filter(s => s.id !== stock.id)})
+
+  changeFilter = (filter) => {
+    filter === 'All'?filter = '':null
+    this.setState({filter})
+  }
+
+  changeSorting = () => this.setState({sortAlphabetically: !this.state.sortAlphabetically})
+
+  getStocks = () => {
+    let stocks = this.state.stocks.filter(s => s.type.includes(this.state.filter))
+    if (this.state.sortAlphabetically) {
+      return stocks.sort((a,b) => a.name > b.name ? 1 : -1)
+    }
+    else {
+      return stocks.sort((a,b) => b.price - a.price)
+    }
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortAlphabetically = {this.state.sortAlphabetically} changeFilter = {this.changeFilter} changeSorting = {this.changeSorting}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks = {this.getStocks()} buyStock = {this.buyStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks = {this.state.portfolio} sellStock = {this.sellStock}/>
 
             </div>
           </div>

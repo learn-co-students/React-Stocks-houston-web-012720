be22497352a177f142f531e19552fb8f8a3d4a5a
DrVonDevious <drvondevious@gmail.com>
2020-04-05 22:05:03 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,22 +4,27 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
-
   render() {
     return (
       <div>
-        <SearchBar/>
-
+        <SearchBar handleSortName={(event) => this.props.handleSortName(event)}
+                   handleSortPrice={(event) => this.props.handleSortPrice(event)}
+                   handleFilter={(event) => this.props.handleFilter(event)}
+                   isSortedByName={this.props.isSortedByName}
+                   isSortedByPrice={this.props.isSortedByPrice}/>
           <div className="row">
             <div className="col-8">
-
-              <StockContainer/>
-
+              <StockContainer stocks={this.props.stocks}
+                              filterType={this.props.filterType}
+                              handleBuy={(stock) => this.props.handleBuy(stock)}
+                              isSortedByName={this.props.isSortedByName}
+                              isSortedByPrice={this.props.isSortedByPrice}/>
             </div>
             <div className="col-4">
-
-              <PortfolioContainer/>
-
+              <PortfolioContainer portfolio={this.props.portfolio} 
+                                  handleSell={(stock) => this.props.handleSell(stock)}
+                                  isSortedByName={this.props.isSortedByName}
+                                  isSortedByPrice={this.props.isSortedByPrice}/>
             </div>
           </div>
       </div>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,13 +3,42 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  sortPortfolio = () => {
+    if (this.props.isSortedByName) {
+      return (
+        this.props.portfolio.sort((a, b) => a.name.localeCompare(b.name))
+          .map(stock => <Stock
+            stock={stock}
+            key={stock.id}
+            handleStock={(stock) => this.props.handleSell(stock)}/>)
+      )
+    } else if (this.props.isSortedByPrice) {
+      return (
+        this.props.portfolio.sort((a, b) => a.price - b.price)
+          .map(stock => <Stock
+            stock={stock}
+            key={stock.id}
+            handleStock={(stock) => this.props.handleSell(stock)}/>)
+      )
+    } else {
+      return (
+        this.props.portfolio.map(stock => <Stock
+          stock={stock}
+          key={stock.id}
+          handleStock={(stock) => this.props.handleSell(stock)}/>)
+      )
+    }
+  }
+
+  filterPortfolio = () => {
+    
+  }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+          {this.sortPortfolio()}
       </div>
     );
   }
